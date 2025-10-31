d36665d8df81562d3d7c39e426bb2636eab27331
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-10-31 10:00:31 +0300

    Make the bottom nav highlight current page

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -4,24 +4,25 @@ import {
 } from "@heroicons/react/16/solid";
 import { HeartIcon } from "@heroicons/react/24/outline";
 import { HomeIcon } from "@heroicons/react/24/solid";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function BottomNav() {
+  const location = useLocation();
   return (
     <nav className="shadow-sm flex gap-6 border-t w-full justify-center">
-      <BtnList path={""}>
+      <BtnList path={""} loc={location.pathname === "/"}>
         <Icon icon={HomeIcon} />
         <BtnName>Home</BtnName>
       </BtnList>
-      <BtnList path={"/about"}>
+      <BtnList path={"/about"} loc={location.pathname === "/about"}>
         <Icon icon={InformationCircleIcon} />
         <BtnName>About</BtnName>
       </BtnList>
-      <BtnList path={"/profile"}>
+      <BtnList path={"/profile"} loc={location.pathname === "/profile"}>
         <Icon icon={UserCircleIcon} />
         <BtnName>Profile</BtnName>
       </BtnList>
-      <BtnList path={"/wishlist"}>
+      <BtnList path={"/wishlist"} loc={location.pathname === "/wishlist"}>
         <Icon icon={HeartIcon} />
         <BtnName>WishList</BtnName>
       </BtnList>
@@ -33,10 +34,14 @@ function Icon({ icon: Icon }) {
   return <Icon className="w-6" />;
 }
 
-function BtnList({ children, path }) {
+function BtnList({ children, path, loc }) {
   return (
     <Link to={`/${path}`} className={`cursor-pointer`}>
-      <div className="flex items-center gap-1 hover:bg-gray-200 p-2">
+      <div
+        className={`flex items-center gap-1 hover:bg-gray-200 p-2 ${
+          loc && "bg-gray-200"
+        }`}
+      >
         {children}
       </div>
     </Link>
