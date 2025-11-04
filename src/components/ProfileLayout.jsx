324b4d77e2937c6ff37a086336963a070750195a
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-11-04 11:20:46 +0300

    Refactor: make a new file for the profile sidebar component and import it

--- src/components/ProfileLayout.jsx
+++ src/components/ProfileLayout.jsx
@@ -1,16 +1,8 @@
-import { HomeIcon } from "@heroicons/react/24/solid";
 import BottomNav from "./BottomNav";
-import BtnList from "./BtnList";
 import Navbar from "./Navbar";
 import Page from "./Page";
+import ProfileSidebar from "./ProfileSidebar";
 import Sticky from "./Sticky";
-import { Icon } from "./Icon";
-import { NavBtnName } from "./NavBtnName";
-import { useLocation } from "react-router-dom";
-import {
-  CalendarDateRangeIcon,
-  InformationCircleIcon,
-} from "@heroicons/react/16/solid";
 
 function ProfileLayout({ children }) {
   return (
@@ -20,7 +12,7 @@ function ProfileLayout({ children }) {
       </Sticky>
 
       <main className="grid grid-cols-[250px_1fr] w-full h-full my-6 overflow-hidden">
-        <Sidebar />
+        <ProfileSidebar />
 
         <div className="p-6 flex flex-col items-start gap-6 overflow-y-auto h-[calc(100vh-8rem)] no-scrollbar">
           {children}
@@ -33,40 +25,4 @@ function ProfileLayout({ children }) {
     </Page>
   );
 }
-
-function Sidebar() {
-  const location = useLocation();
-
-  return (
-    <aside className="flex flex-col gap-2 border-r items-start sticky top-[5rem] h-[calc(100vh-10rem)] overflow-hidden">
-      <BtnList
-        path={"/profilehome"}
-        className={"w-full"}
-        loc={location.pathname === "/profilehome"}
-      >
-        <Icon icon={HomeIcon} />
-        <NavBtnName>Home</NavBtnName>
-      </BtnList>
-
-      <BtnList
-        path={"/profilereservations"}
-        className={"w-full"}
-        loc={location.pathname === "/profilereservations"}
-      >
-        <Icon icon={CalendarDateRangeIcon} />
-        <NavBtnName>Reservations</NavBtnName>
-      </BtnList>
-
-      <BtnList
-        path={"/profileinfo"}
-        className={"w-full"}
-        loc={location.pathname === "/profileinfo"}
-      >
-        <Icon icon={InformationCircleIcon} />
-        <NavBtnName>User Info</NavBtnName>
-      </BtnList>
-    </aside>
-  );
-}
-
 export default ProfileLayout;
---
+++ src/components/ProfileSidebar.jsx
@@ -0,0 +1,46 @@
+import { Icon } from "./Icon";
+import { NavBtnName } from "./NavBtnName";
+import BtnList from "./BtnList";
+import { HomeIcon } from "@heroicons/react/24/solid";
+import {
+  CalendarDateRangeIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/16/solid";
+import { useLocation } from "react-router-dom";
+
+function ProfileSidebar() {
+  const location = useLocation();
+
+  return (
+    <aside className="flex flex-col gap-2 border-r items-start sticky top-[5rem] h-[calc(100vh-10rem)] overflow-hidden">
+      <BtnList
+        path={"/profilehome"}
+        className={"w-full"}
+        loc={location.pathname === "/profilehome"}
+      >
+        <Icon icon={HomeIcon} />
+        <NavBtnName>Home</NavBtnName>
+      </BtnList>
+
+      <BtnList
+        path={"/profilereservations"}
+        className={"w-full"}
+        loc={location.pathname === "/profilereservations"}
+      >
+        <Icon icon={CalendarDateRangeIcon} />
+        <NavBtnName>Reservations</NavBtnName>
+      </BtnList>
+
+      <BtnList
+        path={"/profileinfo"}
+        className={"w-full"}
+        loc={location.pathname === "/profileinfo"}
+      >
+        <Icon icon={InformationCircleIcon} />
+        <NavBtnName>User Info</NavBtnName>
+      </BtnList>
+    </aside>
+  );
+}
+
+export default ProfileSidebar;
