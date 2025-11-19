4feafb34e3a01823fb6845a7cc88e0eaeff9f628
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-11-19 15:50:04 +0300

    Refactor: move the manager navigation to a new file

--- src/components/ManagerLayout.jsx
+++ src/components/ManagerLayout.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import ManagerSidebar from "./ManagerSidebar";
 import {
   ArrowRightStartOnRectangleIcon,
@@ -7,8 +6,13 @@ import {
   UserIcon,
 } from "@heroicons/react/24/outline";
 import { useState } from "react";
+import ManagerLayoutNav from "./ManagerLayoutNav";
 
-function AdminLayout({ children }) {
+function ManagerLayout({ children }) {
+  const [isDark, setIsDark] = useState(false);
+  function handleDarkMode() {
+    setIsDark((dark) => !dark);
+  }
   return (
     <div className="flex w-full h-screen overflow-hidden bg-background2">
       <div className="w-1/5 h-full">
@@ -16,80 +20,11 @@ function AdminLayout({ children }) {
       </div>
 
       <div className="flex flex-col flex-1 h-full">
-        <ManagerNav />
+        <ManagerLayoutNav isDark={isDark} handleDarkMode={handleDarkMode} />
         <main className="flex-1">{children}</main>
       </div>
     </div>
   );
 }
 
-function ManagerNav() {
-  return (
-    <nav className="bg-white border-b flex justify-end w-full">
-      <ManagerProfile />
-      <ManagerNavIcons />
-    </nav>
-  );
-}
-
-function ManagerNavContainer({ children, className }) {
-  return <div className={`${className} flex items-center`}>{children}</div>;
-}
-
-function ManagerProfile() {
-  return (
-    <ManagerNavContainer className={"gap-2 p-3"}>
-      <img
-        src="/profile-pic.jpg"
-        alt="profile picture"
-        className="w-8 rounded-full"
-      />
-      <p className="font-body text-sm">Manager</p>
-    </ManagerNavContainer>
-  );
-}
-
-function ManagerNavIcons() {
-  return (
-    <ManagerNavContainer className="gap-1">
-      <Link
-        to={"/managerprofile"}
-        className="p-1 hover:bg-background rounded-sm"
-      >
-        <Icon icon={UserIcon} />
-      </Link>
-      <LightDarkIcons />
-      <Icon icon={ArrowRightStartOnRectangleIcon} />
-    </ManagerNavContainer>
-  );
-}
-
-// eslint-disable-next-line
-function Icon({ icon: Icon, onClick }) {
-  return (
-    <span
-      className="p-1 cursor-pointer hover:bg-background rounded-sm"
-      onClick={onClick}
-    >
-      <Icon className="w-5 text-primary" />
-    </span>
-  );
-}
-
-function LightDarkIcons() {
-  const [isDark, setIsDark] = useState(false);
-  function handleDarkMode() {
-    setIsDark((dark) => !dark);
-  }
-  return (
-    <>
-      {!isDark ? (
-        <Icon icon={MoonIcon} onClick={handleDarkMode} />
-      ) : (
-        <Icon icon={SunIcon} onClick={handleDarkMode} />
-      )}
-    </>
-  );
-}
-
-export default AdminLayout;
+export default ManagerLayout;
