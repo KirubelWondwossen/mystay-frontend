cf34acbbb54a82b55da35b4b7ba7c4f89454ff88
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-11-24 15:12:09 +0300

    Add the manager home stats cards

--- src/pages/ManagerHome.jsx
+++ src/pages/ManagerHome.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import ManagerLayout from "../components/ManagerLayout";
 import SectionHeader from "../components/SectionHeader";
+import { BriefcaseIcon } from "@heroicons/react/24/outline";
 
 function ManagerHome() {
   return (
     <ManagerLayout>
-      <div class="max-w-[120rem] mx-auto flex flex-col gap-[3.2rem]">
+      <div className="max-w-[120rem] mx-auto flex flex-col gap-[3.2rem]">
         <HomeTop />
+        <ManagerDashboardCards />
       </div>
     </ManagerLayout>
   );
@@ -56,6 +58,77 @@ function FilterDays({ value, activeDay, setActiveDay }) {
     >
       Last {value} days
     </span>
+  );
+}
+
+function ManagerDashboardCards() {
+  return (
+    <div className="grid grid-cols-4 gap-8">
+      <ManagerDashboardCard>
+        <ManagerStats
+          icon={BriefcaseIcon}
+          stat={"Booking"}
+          value={11}
+          iconColor={"text-[#0369a1]"}
+          iconBg={"bg-[#e0f2fe]"}
+        />
+      </ManagerDashboardCard>
+      <ManagerDashboardCard>
+        <ManagerStats
+          icon={BriefcaseIcon}
+          stat={"Sales"}
+          value={"$32,465.00"}
+          iconColor={"text-[#15803d]"}
+          iconBg={"bg-[#dcfce7]"}
+        />
+      </ManagerDashboardCard>
+      <ManagerDashboardCard>
+        <ManagerStats
+          icon={BriefcaseIcon}
+          stat={"Check ins"}
+          value={4}
+          iconColor={"text-[#4338ca]"}
+          iconBg={"bg-[#e0e7ff]"}
+        />
+      </ManagerDashboardCard>
+      <ManagerDashboardCard>
+        <ManagerStats
+          icon={BriefcaseIcon}
+          stat={"Occupancy rate"}
+          value={"36%"}
+          iconColor={"text-[#a16207]"}
+          iconBg={"bg-[#fef9c3]"}
+        />
+      </ManagerDashboardCard>
+    </div>
+  );
+}
+
+function ManagerDashboardCard({ children }) {
+  return (
+    <div className="border bg-white borde-border rounded-md w-full">
+      {children}
+    </div>
+  );
+}
+
+// eslint-disable-next-line
+function ManagerStats({ icon: Icon, stat, value, iconColor, iconBg }) {
+  return (
+    <div className="grid grid-cols-[4rem_1fr] grid-rows-2 px-3 py-2 w-max gap-x-1">
+      <div
+        className={`p-4 self-center justify-self-start aspect-square row-span-full rounded-full flex items-center justify-center text-start ${iconBg}`}
+      >
+        <Icon className={`w-7 ${iconColor}`} />
+      </div>
+      <p className="text-[#6b7280] justify-self-start text-start self-end text-xs uppercase font-semibold font-body">
+        {stat}
+      </p>
+
+      <p className="font-heading text-xl text-start justify-self-start min-w-0 break-words">
+        {value}
+      </p>
+    </div>
   );
 }
 
