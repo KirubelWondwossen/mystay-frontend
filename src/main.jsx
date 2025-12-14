2ae7ffb07dafe3246a0ad9643b7d618993cb5b40
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-12-15 00:26:47 +0300

    Install the react-hot-toaster

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "./chartConfig";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
