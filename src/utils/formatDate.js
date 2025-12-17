26c736d46a552b7055b474177be5d470003309a7
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-12-17 18:00:42 +0300

    Add a function tha convert parse the ISO date and date and time format it

---
+++ src/utils/formatDate.js
@@ -0,0 +1,20 @@
+export function formatDate(dateString) {
+  const date = new Date(dateString);
+
+  const formattedDate = date.toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  const formattedTime = date.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
+
+  return {
+    date: formattedDate,
+    time: formattedTime,
+  };
+}
